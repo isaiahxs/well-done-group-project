11cd1129ee1c6c4cd3482976ef266f8b0bc5ce73
isaiah <isaiahsinnathamby@gmail.com>
2023-05-24 20:21:53 -0400

    added story.py

---
+++ app/models/story.py
@@ -0,0 +1,30 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+from sqlalchemy import Column, DateTime, func
+
+class Story(db.Model):
+    __tablename__ = 'stories'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    author = db.relationship('User', backref='stories')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'author_id': self.author_id,
+            'title': self.title,
+            'content': self.content,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
