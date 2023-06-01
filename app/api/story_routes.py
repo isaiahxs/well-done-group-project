dd92de0ef22c67c2ee22034e9a15722cc36170eb
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-05-31 19:21:12 -0700

    created getCurrUser, getAllStories, getStoryById, DeleteStoryById routes compelte. started createNewStory

--- app/api/story_routes.py
+++ app/api/story_routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, session, render_template, request, redirect
 from flask_login import login_required
-from app.models import Story
-
+from app.models import db, Story
+from app.forms import StoryForm
 story_routes = Blueprint('stories', __name__)
 
 
@@ -11,9 +11,8 @@ def stories():
     Query for all stories and returns them in a list of story dictionaries
     """
 
+    stories = Story.query.all()
     return {'stories': [story.to_dict() for story in stories]}
-    # stories = Story.query.all()
-    # return {'stories': [story.to_dict() for story in stories]}
 
 
 @story_routes.route('/<int:id>')
@@ -23,3 +22,46 @@ def story(id):
     """
     story = Story.query.get(id)
     return story.to_dict()
+
+
+
+@story_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_story(id):
+    """
+    Deletes a story by id
+    """
+    story = Story.query.get(id)
+    if story is None:
+        return {"error": "Story not found"}, 404
+
+    db.session.delete(story)
+    db.session.commit()
+
+    return {"message": "Story deleted successfully"}
+
+
+
+@story_routes.route('/', methods=['POST'])
+@login_required
+def create_story():
+    """
+    Creates a new story
+    """
+    form = StoryForm
+    if form.validate_on_submit():
+      data = form.data
+      new_story = Story(
+          author_id=data['author_id'],
+          title=data['title'],
+          content=data['content']
+      )
+      db.session.add(new_story)
+      db.session.commit()
+      return new_story.to_dict()
+      return redirect("/story/<int:id>")
+
+    if form.errors:
+      return "Bad Data"
+
+ 
