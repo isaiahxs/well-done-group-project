69e3bf05eadc2713b7e1be30ccf33b94e12dd40f
isaiah <isaiahsinnathamby@gmail.com>
2023-06-12 16:07:21 -0400

    modified options in StoryPage to have svgs - going to hide Unclap and get us a favicon

--- react-app/src/components/CommentPanel/index.js
+++ react-app/src/components/CommentPanel/index.js
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import Comments from '../Comments';
 import { useSelector } from 'react-redux';
 import './CommentPanel.css'; 
+import comment from '../../public/comment.svg';
 
 const CommentPanel = ({ showComments, setShowComments, story}) => {
 
@@ -9,13 +10,16 @@ const CommentPanel = ({ showComments, setShowComments, story}) => {
     const [commentBtnText, setCommentBtnText] = useState('Comments')
 
     useEffect(()=>{
-        setCommentBtnText(story.comments.length ? `Comments ${story.comments.length}` : 'Comments')
-        
+        // setCommentBtnText(story.comments.length ? `Comments ${story.comments.length}` : 'Comments')
+        setCommentBtnText(story.comments.length)
     }, [story])
 
     return (
         <div>
-            <button onClick={() => setShowComments(!showComments)}>{commentBtnText}</button> 
+            <button className="comment-button" onClick={() => setShowComments(!showComments)}>
+                <img src={comment} alt="comment" className="comment-icon" />
+                {commentBtnText}
+            </button> 
 
             <div className={`comment-panel ${showComments ? 'comment-panel-open' : 'comment-panel-closed'}`}>
                 <Comments storyId={story.id} authorInfo={story.authorInfo} userId={userId}/>
